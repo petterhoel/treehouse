deacd4cfb442fcb85bf1dd341aea28f04537d4dc
Petter Hoel <petter@petterhoel.net>
2014-06-11 09:40:33 +0200

    finished lightbox

--- creating a simple lightbox/js/app.js
+++ creating a simple lightbox/js/app.js
@@ -3,26 +3,33 @@
 // Solution: create an overlay with the large image - lightbox
 
 var $overlay = $('<div id="overlay"></div>');
-var $image = $('<img>');
-// Add an overlay
+var $image = $("<img>");
+var $caption = $("<p></p>");
+
+// Add an image to overlay
 $overlay.append($image);
+// Add a caption to overlay
+$overlay.append($caption);
+// Add an overlay
 $("body").append($overlay);
-  // An image
-  // A caption
-//1. Capture click event on a link to an image
+
+//Capture click event on a link to an image
 $("#imageGallery a").click(function(event){
   event.preventDefault();
   var imageLocation = $(this).attr("href");
   // Update overlay with the image linked in the link
   $image.attr("src", imageLocation);
   // Show the overlay
-  
   $overlay.show();
-});
+
   // Get  childs alt attribute and set caption
+  var captionText = $(this).children("img").attr("alt");
+  $caption.text(captionText);
+});
+  
 
 //When overlay is clicked 
-$overlay.click.(function(){
+$overlay.click(function(){
   //hide the overlay
   $overlay.hide();
 });
