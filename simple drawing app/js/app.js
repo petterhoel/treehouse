cb9e4a8d4dab3db3847804e44fcc73aa8952a915
Petter Hoel <petter@petterhoel.net>
2014-06-12 18:17:42 +0200

    project planned with pseudo code

---
+++ simple drawing app/js/app.js
@@ -0,0 +1,20 @@
+// Problem: No user interaction causes no change to application
+// Solution: When user interacts cause changes approperiately
+
+
+// when clicking on control list items
+	// deselect sibling elements
+	// select clicked element
+
+// when add color is pressed 
+	// Show color select or hide the color select
+
+// When color slider change 
+	// update new color span
+
+// When add color is pressed
+	// Append the color to the controls ul
+	// Select the new color
+
+// On mouse events on the canvas 
+	// draw lines
