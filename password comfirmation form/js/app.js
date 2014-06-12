13138dea977ed1723c5002cd3eefa03ebfaced3f
Petter Hoel <petter@petterhoel.net>
2014-06-12 15:03:29 +0200

    pseudo code for fix added

---
+++ password comfirmation form/js/app.js
@@ -0,0 +1,14 @@
+// Problem: hints are shown even when form is valif
+// Solution hide and show them at approriate times
+
+// Hide hints
+
+// When event happens on password input
+	// find out if password is valid
+		// hide hint if valid 
+		// else show hints
+
+// When event happpens on confirmation input
+	// find out if password and confirmation match
+		// hide hint if match
+		// else show hint
