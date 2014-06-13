4c0376492c51c67a057c51e0a992ae82ba922568
Petter Hoel <petter@petterhoel.net>
2014-06-13 09:16:34 +0200

    drawing app done

--- simple drawing app/js/app.js
+++ simple drawing app/js/app.js
@@ -2,6 +2,10 @@
 // Solution: When user interacts cause changes approperiately
 
 var color = $(".selected").css("background-color");
+var $canvas = $("canvas");
+var context = $("canvas")[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
 
 // when clicking on control list items
 $(".controls").on("click", "li", function() {
@@ -49,4 +53,21 @@ $("#addNewColor").click(function(){
 
 
 // On mouse events on the canvas 
+$canvas.mousedown(function(e){
+	lastEvent = e;
+	mouseDown = true;
+}).mousemove(function(e){
 	// draw lines
+	if(mouseDown){
+		context.beginPath();
+		context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+		context.lineTo(e.offsetX, e.offsetY);
+		context.strokeStyle = color;
+		context.stroke();
+		lastEvent = e;
+	}
+}).mouseup(function(){
+	mouseDown = false;
+}).mouseleave(function(){
+	$canvas.mouseup();
+});
