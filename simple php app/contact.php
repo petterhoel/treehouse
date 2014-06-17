76d56f4579d31ff73b5f6f7ea0e4cef44506aa26
Petter Hoel <petter@petterhoel.net>
2014-06-17 11:00:23 +0200

    contact form added

--- simple php app/contact.php
+++ simple php app/contact.php
@@ -4,8 +4,41 @@ $section = "contact";
 include('includes/header.php'); ?>
 
 	<div class="section page">
-		
-		<h1>Contact</h1>
+		<div class="wrapper">
+			<h1>Contact</h1>
+			
+			<p>I'd love to hear from you! Complete the form to send me an e-mail. </p>
+
+			<form method="post">
+				<table>
+					<tr>
+						<th>
+							<label for="name">Name </label>
+						</th>
+						<td>
+							<input type="text" name="name" id="name">
+						</td>
+					</tr>
+					<tr>
+						<th>
+							<label for="email">Email </label>
+						</th>
+						<td>
+							<input type="text" name="email" id="email">
+						</td>
+					</tr>
+					<tr>
+						<th>
+							<label for="message">Message </label>
+						</th>
+						<td>
+							<textarea name="message" id="message"> </textarea>
+						</td>
+					</tr>
+				</table>
+				<input type="submit" value="Send">
+			</form>
+		</div>
 
 
 	</div>
