537dfa67e697d8e71787659714fa1a0a6377defe
Petter Hoel <petter@petterhoel.net>
2014-06-18 20:38:43 +0200

    more arrays

--- simple php app/arrays.php
+++ simple php app/arrays.php
@@ -15,7 +15,56 @@
  	foreach($flavours as $flavour){
  		echo $flavour . "\n";
  	} 
- 	?>
+
+$countries = array();
+$countries[0] = array(
+	"code" => "US",
+	"name" => "Unitied States",
+	"capital" => "Washington, D.C.", 
+	"population" => 225000000,
+	"anthem" => "The Star-Sprangled Banner"
+);
+
+$countries[1] = array(
+	"code" => "DE",
+	"name" => "Germany",
+	"capital" => "Berlin", 
+	"population" => 81799600,
+	"anthem" => "Song of the Germans"
+);
+
+?>
+// <pre> <?php var_dump($countries); ?>	</pre>
 
 
- </pre>
+<?php
+/*
+// assosiative array
+$country = array(
+	"code" => "US",
+	"name" => "Unitied States",
+	"capital" => "Washington, D.C.", 
+	"population" => 225000000,
+	"anthem" => "The Star-Sprangled Banner"
+
+);
+*/
+?>
+</pre>
+<html>
+<body>
+<?php foreach ($countries as $country) { ?>
+
+	<h1><?php echo $country["name"]; ?></h1>
+
+	<dl>
+		<dt>Country code</dt>
+		<dd><?php echo $country["code"]; ?></dd>
+		<dt>Capital</dt>
+		<dd><?php echo $country["capital"];?></dd>
+		<dt>Population</dt>
+		<dd><?php echo $country["population"];?></dd>
+	</dl>
+<?php } ?>
+</body>
+</html>
--- simple php app/shirts.php
+++ simple php app/shirts.php
@@ -1,3 +1,14 @@
+<?php
+
+$products =array();
+$products[101] = "Logo Shirt, Red"; 
+$products[102] = "Mike the Frog Shirt, Black"; 
+$products[103] = "Mike the Frog Shirt, Blue";
+$products[104] = "Logo Shirt, Green";
+
+?>
+
+
 <?php 
 $pageTitle = "Mike's Full Catalog of Shirts";
 $section = "shirts";
@@ -5,10 +16,19 @@ include('includes/header.php');
 ?>
 
 	<div class="section page">
-		
-		<h1>Mike's Full Catalog of Shirts</h1>
+		<div class="wrapper">
+
+			<h1>Mike's Full Catalog of Shirts</h1>
+
+			<ul>
+				<?php foreach ($products as $product) { ?>
+						<li><?php echo $product; ?></li>
+				<?php	}?>
+
+			</ul>
 
 
+		</div>
 	</div>
 
 
