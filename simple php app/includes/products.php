9f45d49f7ecb242e85647b4dc6dab30d46890b72
Petter Hoel <petter@petterhoel.net>
2014-06-20 13:17:13 +0200

    form content and anti bot validation

--- simple php app/includes/products.php
+++ simple php app/includes/products.php
@@ -1,7 +1,6 @@
 <?php
 
 function get_list_view_html($product_id, $product){
-
     $output = '<li>';
     $output = $output . '<a href="shirt.php?id=' . $product_id . '">';
     $output = $output . '<img src="' . $product["img"] . '" alt="' . $product["name"] . '">';
@@ -10,11 +9,7 @@ function get_list_view_html($product_id, $product){
     $output = $output . '</a>';
     $output = $output . '</li>';
     return $output;
-
 }
-
-
-
 $products =array();
 $products[101] = array(
 	"name" => "Logo Shirt, Red",
